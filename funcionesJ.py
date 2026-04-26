366e827c6a7e20c0d73f316f270813ea4f21cde6
Joel-tec26 <j.porras.4@estudiantec.cr>
2026-04-25 23:51:46 -0600

    feat(tokens): agregue  módulo de carga y procesamiento de tokens  desde archivo >> >> - Se implementa validación de separador de tokens >> - Se añade verificación de existencia de archivo >> - Se desarrolla lectura y procesamiento de archivo de tokens >> - Se construye flujo interactivo para carga para un  menú posterior >> - Se integran funciones auxiliares para validación y de entradas

---
+++ funcionesJ.py
@@ -0,0 +1,86 @@
+#Creado por: Joel Jesús Porras Muñoz y Alexis Torres
+# Fecha de creación: 24/04/2026 9:30pm
+# Ultíma modificación: 
+# Versión: 3.14
+
+# importaciones de metodos
+import re
+import os
+
+# definicion de funciones 
+def validardivision (pcaracter):
+    """
+    
+    """
+    patron = r'^[^\d\s\w]$'
+    return bool(re.match(patron, pcaracter))
+
+def validarExistencia(pruta):
+    """
+
+    """
+    if os.path.exists(pruta):
+        return True
+    else:
+        print(f"No se encontró el archivo {pruta} en los archivos.")
+        return False
+
+# funciones opción 1 del menú 
+
+def solicitarCargaTokens():
+    """
+    
+    """
+    print ("="*30)
+    print ("Opción 1")
+    print ("="*30)
+    print("\n--- Configuración de Carga de Diccionario ---")
+    pruta = input("\nPor favor, ingrese el nombre del archivo de tokens: ")
+    while True:
+        pseparador = input("Ingrese el símbolo que separa las palabras (solo 1 símbolo): ")
+        if validardivision(pseparador):
+            print("Separador aceptado")
+            return pruta, pseparador
+        else:
+            print("El separador debe ser exactamente un símbolo (ni letras, ni números, ni espacios.")
+        
+
+
+def procesarArchivo1(pruta, pseparador):
+    """
+
+    """
+    listaTokens = []
+    archivo = open(pruta, "r", estandar="utf-8")
+    lineaActual = archivo.readline()
+    while lineaActual != "":
+        lineaLimpia = lineaActual.strip()
+        if lineaLimpia != "" and pseparador in lineaLimpia:
+            partesLinea = lineaLimpia.split(pseparador)
+            parejaTokens = (partesLinea[0].strip(), partesLinea[1].strip())
+            listaTokens.append(parejaTokens)
+        lineaActual = archivo.readline() 
+    archivo.close()
+    return listaTokens
+
+def administradorOpcion1():
+    """
+    """
+    while True:
+        ruta, separador = solicitarCargaTokens()
+        if validarExistencia(ruta):
+            try:
+                tokens = procesarArchivo1(ruta, separador)
+                print(f"\tCarga completada: {len(tokens)} tokens encontrados")
+                return tokens 
+            except Exception :
+                print(" Caracteristica inesperada encontrada al leer el archivo")
+        opcion = input("\n¿Desea intentar de nuevo con otra ruta? \nDigite (1) para continuar \nDigite (2) para salir: ")
+        if opcion != "1":
+            print("Operación cancelada.")
+            return []
+
+
+# Programa principal de pruebas
+
+lista = administradorOpcion1()
